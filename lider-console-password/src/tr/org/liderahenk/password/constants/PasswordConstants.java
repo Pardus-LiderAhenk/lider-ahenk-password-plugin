ae7de4c348e9d84f81822bd6620246ccf711b839
emrekgn <emre.akkaya@agem.com.tr>
2016-08-16 10:31:51 +0300

    Initial commit

---
+++ lider-console-password/src/tr/org/liderahenk/password/constants/PasswordConstants.java
@@ -0,0 +1,9 @@
+package tr.org.liderahenk.password.constants;
+
+public class PasswordConstants {
+	
+	public static final String PLUGIN_NAME = "password";
+	
+	public static final String PLUGIN_VERSION = "1.0.0";
+	
+}
