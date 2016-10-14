9aed4e1d0881fe8bc85d4d9362f9177a9aaba513
cemrealpsoy <cemrealpsoy@gmail.com>
2016-10-14 11:42:53 +0300

    admin privilege added to task

--- lider-console-password/src/tr/org/liderahenk/password/handlers/PasswordTaskHandler.java
+++ lider-console-password/src/tr/org/liderahenk/password/handlers/PasswordTaskHandler.java
@@ -5,14 +5,22 @@ import java.util.Set;
 import org.eclipse.swt.widgets.Display;
 
 import tr.org.liderahenk.liderconsole.core.handlers.MultipleSelectionHandler;
+import tr.org.liderahenk.liderconsole.core.ldap.utils.LdapUtils;
+import tr.org.liderahenk.liderconsole.core.current.UserSettings;
+import tr.org.liderahenk.liderconsole.core.widgets.Notifier;
 import tr.org.liderahenk.password.dialogs.PasswordTaskDialog;
+import tr.org.liderahenk.password.i18n.Messages;
 
 public class PasswordTaskHandler extends MultipleSelectionHandler {
 
 	@Override
 	public void executeWithDNSet(Set<String> dnSet) {
+		if (!LdapUtils.getInstance().isAdmin(UserSettings.USER_DN)) {
+		    Notifier.error(null, Messages.getString("NEED_ADMIN_PRIVILEGE"));
+		    return;
+		}
 		PasswordTaskDialog dialog = new PasswordTaskDialog(Display.getDefault().getActiveShell(), dnSet, true);
 		dialog.create();
-		dialog.open();
+		dialog.openWithEventBroker();
 	}
 }
