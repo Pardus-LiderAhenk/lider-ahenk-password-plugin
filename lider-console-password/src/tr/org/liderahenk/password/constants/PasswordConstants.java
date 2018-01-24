8ec6a36be84d7f8dacb4d65143f17b27baaf6532
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2018-01-24 16:43:21 +0300

    version name fix: 1.1

--- lider-console-password/src/tr/org/liderahenk/password/constants/PasswordConstants.java
+++ lider-console-password/src/tr/org/liderahenk/password/constants/PasswordConstants.java
@@ -4,7 +4,7 @@ public class PasswordConstants {
 	
 	public static final String PLUGIN_NAME = "password";
 	
-	public static final String PLUGIN_VERSION = "1.0.0";
+	public static final String PLUGIN_VERSION = "1.1";
 
 	public static final String PASSWORD = "password";
 	
